022739056e25f001f2e50ccb2b50d66f6b306049
Alec Caputo <acaputo@mail.smcvt.edu>
2021-09-30 17:59:52 -0600

    language

--- index.ts
+++ index.ts
@@ -39,8 +39,8 @@ client.on('message', async (message) => {
         }
         lastMessageTime = message.createdTimestamp;
         const {context, command} = parseServerCommand(message.content);
-        if (command === 'queuetime' && context) {
-            message.channel.send(`Looking stats for ${context}, this may take a while...`);
+        if (command === 'stats' && context) {
+            message.channel.send(`Looking up stats for ${context}, this may take a while...`);
             const stats = await getNewWorldStatusHtml(context);
             if (stats) {
                 message.channel.send(`Stats for ${stats.name}:\nQueue: ${stats.population} | Wait Time: ${stats.waitTime}`)
@@ -57,25 +57,33 @@ interface IServerStatus {
     name: string;
 }
 const getNewWorldStatusHtml = async (serverName: string): Promise<IServerStatus | null> => {
-    const url = 'https://newworldstatus.com/';
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.waitForSelector('.text-end');
-    const stuff = await page.evaluate(() => {
-        let rows: any[] = [];
-        window.document.querySelectorAll("table#db76b9e516bd > tbody > tr").forEach(rowNode => { const el: (string | null)[] = []; rowNode.querySelectorAll('td').forEach(cell => el.push(cell.textContent)); rows.push(el) })
-        return rows;
-    });
-    const myServer = stuff.find(x => x[1].toLocaleLowerCase() === serverName.toLocaleLowerCase());
-    console.log(myServer);
-    if (!myServer || !myServer.length) {
+    try {
+        const url = 'https://newworldstatus.com/';
+        const browser = await puppeteer.launch({headless: true});
+        const page = await browser.newPage();
+        await page.goto(url);
+        await page.waitForSelector('.text-end');
+        const stuff = await page.evaluate(() => {
+            let rows: any[] = [];
+            window.document.querySelectorAll("table#db76b9e516bd > tbody > tr").forEach(rowNode => {
+                const el: (string | null)[] = [];
+                rowNode.querySelectorAll('td').forEach(cell => el.push(cell.textContent));
+                rows.push(el)
+            })
+            return rows;
+        });
+        const myServer = stuff.find(x => x[1].toLocaleLowerCase() === serverName.toLocaleLowerCase());
+        console.log(myServer);
+        if (!myServer || !myServer.length) {
+            return null;
+        }
+        return {
+            name: myServer[1] as string,
+            population: myServer[5] as string,
+            waitTime: myServer[6] as string
+        } as IServerStatus
+    } catch (e) {
         return null;
     }
-    return {
-        name: myServer[1] as string,
-        population: myServer[5] as string,
-        waitTime: myServer[6] as string
-    } as IServerStatus
 }
 
