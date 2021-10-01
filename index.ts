7dba8d7c8e53b44b0ca4ccd0bdb3467d0a3bc5b5
Alec Caputo <acaputo@mail.smcvt.edu>
2021-10-01 08:56:25 -0600

    trying stuff to get it to work on puppeteer

--- index.ts
+++ index.ts
@@ -64,8 +64,8 @@ const getNewWorldStatusHtml = async (serverName: string): Promise<IServerStatus 
             args: ['--no-sandbox','--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
-        await page.goto(url);
-        await page.waitForSelector('.text-end', {timeout: 12000});
+        await page.goto(url, {timeout: 20000, waitUntil: 'networkidle0'});
+        // await page.waitForSelector('.text-end', {timeout: 12000});
         const stuff = await page.evaluate(() => {
             let rows: any[] = [];
             window.document.querySelectorAll("table#db76b9e516bd > tbody > tr").forEach(rowNode => {
